2ab475cadfb2f0e2e9afbfb8eda393d41e84c2aa
kamlesh verma <kamleshverma@kamleshs-MacBook-Pro.local>
2024-02-05 14:48:20 +0530

    sdfdsf

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
 import { Link, useNavigate } from "react-router-dom";
 import Logo from "./Logo";
 import { SessionContext } from "../providers/SessionProvider";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import NotificationDropdown from "./NotificationDropdown";
 import UserAvatar from "./UserAvatar";
 import { EventSourceContext } from "../providers/EventSourceProvider";
+import { apiCall } from "../utils";
+import Loading from "./Loading";
 
 const Header = () => {
   const { loggedInUser, setLoggedInUser } = useContext(SessionContext);
   const { eventSource } = useContext(EventSourceContext);
   const navigate = useNavigate();
+  const [autocomplete, setAutocomplete] = useState("");
+  const [foundUsers, setFoundUsers] = useState([]);
+  const [isUserFindLoading, setIsUserFindLoading] = useState(false);
 
   const logout = async () => {
     if (eventSource) {
@@ -19,6 +24,25 @@ const Header = () => {
     localStorage.removeItem("user");
     setLoggedInUser(null);
   };
+
+  useEffect(() => {
+    (async () => {
+      if (autocomplete) {
+        setIsUserFindLoading(true);
+        const res = await apiCall({
+          url: `${process.env.REACT_APP_SERVER_END_PONT}/autocomplete/${autocomplete}`,
+        });
+        if (res) {
+          setFoundUsers(res.users);
+        }
+        setIsUserFindLoading(false);
+      }
+    })();
+
+    return () => {
+      setIsUserFindLoading(false);
+    };
+  }, [autocomplete]);
 
   return (
     <nav
@@ -32,6 +56,64 @@ const Header = () => {
         <div className="d-flex">
           {loggedInUser ? (
             <div className="d-flex align-items-center">
+              <div style={{ marginRight: "15px" }}>
+                <div className="dropdown">
+                  <div data-bs-toggle="dropdown" aria-expanded="false">
+                    <form onSubmit={() => {}}>
+                      <input
+                        className="form-control me-2"
+                        id="findUserDropDown"
+                        placeholder="Search for friends"
+                        aria-label="Search"
+                        value={autocomplete}
+                        onChange={(e) => {
+                          setAutocomplete(e.target.value);
+                        }}
+                      ></input>
+                    </form>
+                  </div>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="findUserDropDown"
+                  >
+                    {isUserFindLoading ? (
+                      <li style={{ textAlign: "center" }}>
+                        <Loading />
+                      </li>
+                    ) : (
+                      <>
+                        {foundUsers.map((user) => {
+                          return (
+                            <Link key={user._id} to={`/profile/${user._id}`}>
+                              <div
+                                style={{
+                                  display: "flex",
+                                  alignItems: "center",
+                                  padding: "10px",
+                                }}
+                              >
+                                <UserAvatar
+                                  profilePicURL={user.profilePicURL}
+                                />
+                                <div style={{ marginLeft: "5px" }}>
+                                  {user.name}
+                                </div>
+                              </div>
+                            </Link>
+                          );
+                        })}
+                        {!!autocomplete &&
+                          !isUserFindLoading &&
+                          foundUsers.length === 0 && (
+                            <li style={{ textAlign: "center" }}>
+                              <h6>User not found!</h6>
+                            </li>
+                          )}
+                      </>
+                    )}
+                  </ul>
+                </div>
+              </div>
               <div style={{ marginRight: "15px" }}>
                 <div
                   style={{

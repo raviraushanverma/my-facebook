008c563c0acb8148e785fc8e15c027a29346410b
kamlesh verma <kamleshverma@kamleshs-MacBook-Pro.local>
2024-02-07 16:26:02 +0530

    donererre

--- src/components/FriendChatList.js
+++ src/components/FriendChatList.js
@@ -8,44 +8,54 @@ const FriendChatList = () => {
     return null;
   }
 
+  const friends = Object.values(loggedInUser.friends).filter((friend) => {
+    return friend.state === "FRIEND_REQUEST_CONFIRM";
+  });
+
+  if (!friends.length) {
+    return null;
+  }
+
   return (
     <div className="d-none d-md-block chat-card contacts_card">
-      <div className="chat-card-header">
-        <div className="input-group">
-          <input
-            type="text"
-            placeholder="Search Friends..."
-            name=""
-            className="form-control search"
-          />
-          <div className="d-flex">
-            <span className="input-group-text search_btn">
-              <i className="fas fa-search"></i>
-            </span>
+      {friends.length > 7 && (
+        <div className="chat-card-header">
+          <div className="input-group">
+            <input
+              type="text"
+              placeholder="Search Friends..."
+              name=""
+              className="form-control search"
+            />
+            <div className="d-flex">
+              <span className="input-group-text search_btn">
+                <i className="fas fa-search"></i>
+              </span>
+            </div>
           </div>
         </div>
-      </div>
+      )}
       <div className="card-body contacts_body">
         <ul className="contacts">
-          {Object.values(loggedInUser.friends).map((friend) => {
+          {friends.map((friend) => {
             return (
               <div key={friend.user._id}>
-                {friend.state === "FRIEND_REQUEST_CONFIRM" && (
-                  <li className="online">
-                    <div className="d-flex" style={{ marginTop: "7px" }}>
-                      <div className="img_cont">
-                        <UserAvatar profilePicURL={friend.user.profilePicURL} />
-                        <span className="online_icon"></span>
-                      </div>
-                      <div className="user_info">
+                <li className="online">
+                  <div className="d-flex" style={{ marginTop: "7px" }}>
+                    <div className="img_cont">
+                      <UserAvatar profilePicURL={friend.user.profilePicURL} />
+                      <span className="online_icon"></span>
+                    </div>
+                    <div className="user_info">
+                      <div>
                         <span>
                           <h6>{friend.user.name}</h6>
                         </span>
                         <p>{friend.user.name} is online</p>
                       </div>
                     </div>
-                  </li>
-                )}
+                  </div>
+                </li>
               </div>
             );
           })}

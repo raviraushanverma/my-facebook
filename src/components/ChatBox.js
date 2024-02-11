cb4f0ed41e608a0bc02322df67669629fdddc0f4
ns-kkumarverma <kkumarverma@netskope.com>
2024-02-11 13:33:55 +0530

    sdfsfds

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -6,6 +6,7 @@ import { SessionContext } from "../providers/SessionProvider";
 import { WebsocketContext } from "../providers/WebsocketProvider";
 import { ActiveChatMessageContext } from "../providers/ActiveChatMessageProvider";
 import TimeAgo from "javascript-time-ago";
+import OutsideClickHandler from "react-outside-click-handler";
 
 const ChatBox = () => {
   const timeAgo = new TimeAgo("en-US");
@@ -24,7 +25,6 @@ const ChatBox = () => {
   useEffect(() => {
     chatContainer?.current?.scrollIntoView({
       block: "end",
-      behavior: "smooth",
     });
   }, [isMessageTyping, activeChatMessages]);
 
@@ -115,77 +115,82 @@ const ChatBox = () => {
   }
 
   return (
-    <div className="card chat-box">
-      <div className="card-header msg_head">
-        <div className="d-flex bd-highlight">
-          <div className="d-flex">
-            <div className="img_cont">
-              <Link to={`/profile/${currentUser._id}`} title="Account">
-                <UserAvatar profilePicURL={currentUser.profilePicURL} />
-                <span
-                  className={`status_icon ${
-                    currentUser.isOnline ? "online" : ""
-                  }`}
-                ></span>
-              </Link>
-              <span className="online_icon" />
+    <OutsideClickHandler
+      onOutsideClick={() => {
+        setActiveChatFriendId(null);
+      }}
+    >
+      <div className="card chat-box">
+        <div className="card-header msg_head">
+          <div className="d-flex bd-highlight">
+            <div className="d-flex">
+              <div className="img_cont">
+                <Link
+                  to={`/profile/${currentUser._id}`}
+                  title="Account"
+                  onClick={() => setActiveChatFriendId(null)}
+                >
+                  <UserAvatar profilePicURL={currentUser.profilePicURL} />
+                  <span
+                    className={`status_icon ${
+                      currentUser.isOnline ? "online" : ""
+                    }`}
+                  ></span>
+                </Link>
+                <span className="online_icon" />
+              </div>
+              <div className="user_info">
+                <span> &nbsp; {currentUser.name}</span>
+              </div>
             </div>
-            <div className="user_info">
-              <span> &nbsp; {currentUser.name}</span>
+            <div className="video_cam">
+              <span>
+                <i className="fas fa-video" />
+              </span>
+              <span>
+                <i className="fas fa-phone" />
+              </span>
             </div>
-          </div>
-          <div className="video_cam">
-            <span>
-              <i className="fas fa-video" />
-            </span>
-            <span>
-              <i className="fas fa-phone" />
-            </span>
-          </div>
-          <div className="chat-box-close-button">
-            <button
-              type="button"
-              className="btn-close"
-              aria-label="Close"
-              onClick={() => {
-                setActiveChatFriendId(null);
-              }}
-            ></button>
+            <div className="chat-box-close-button">
+              <button
+                type="button"
+                className="btn-close"
+                aria-label="Close"
+                onClick={() => {
+                  setActiveChatFriendId(null);
+                }}
+              ></button>
+            </div>
           </div>
         </div>
-      </div>
-      <div
-        className={`card-body msg_card_body ${
-          isChatLoading || !activeChatMessages.length
-            ? "complete-center gray-out"
-            : ""
-        }`}
-      >
-        <div ref={chatContainer}>
-          <>
-            {isChatLoading ? (
-              "Loading..."
-            ) : (
-              <>
-                {!activeChatMessages.length && "There are no conversations"}
-                {activeChatMessages.map((chat) => {
-                  if (chat.from === loggedInUser._id) {
-                    return (
-                      <div
-                        key={chat.time}
-                        className="d-flex justify-content-end mb-4"
-                      >
-                        <div className="msg_cotainer_send">
-                          {chat.message}
-                          <span className="msg_time_send">
-                            {timeAgo.format(new Date(chat.time))}
-                          </span>
-                        </div>
-                        <div className="img_cont_msg">
-                          <Link
-                            to={`/profile/${loggedInUser._id}`}
-                            title="Account"
-                          >
+        <div
+          className={`card-body msg_card_body ${
+            isChatLoading || !activeChatMessages.length
+              ? "complete-center gray-out"
+              : ""
+          }`}
+        >
+          <div ref={chatContainer}>
+            <>
+              {isChatLoading ? (
+                "Loading..."
+              ) : (
+                <>
+                  {!activeChatMessages.length && "There are no conversations"}
+                  {activeChatMessages.map((chat) => {
+                    if (chat.from === loggedInUser._id) {
+                      return (
+                        <div
+                          key={chat.time}
+                          className="d-flex justify-content-end mb-4"
+                        >
+                          <div className="msg_cotainer_send">
+                            {chat.message}
+                            <span className="msg_time_send">
+                              {timeAgo.format(new Date(chat.time))}
+                            </span>
+                          </div>
+                          <div className="img_cont_msg">
                             <UserAvatar
                               profilePicURL={loggedInUser.profilePicURL}
                               styleForUserAvatar={{
@@ -196,21 +201,16 @@ const ChatBox = () => {
                                 fontSize: "1em",
                               }}
                             />
-                          </Link>
+                          </div>
                         </div>
-                      </div>
-                    );
-                  } else {
-                    return (
-                      <div
-                        key={chat.time}
-                        className="d-flex justify-content-start mb-4"
-                      >
-                        <div className="img_cont_msg">
-                          <Link
-                            to={`/profile/${currentUser._id}`}
-                            title="Account"
-                          >
+                      );
+                    } else {
+                      return (
+                        <div
+                          key={chat.time}
+                          className="d-flex justify-content-start mb-4"
+                        >
+                          <div className="img_cont_msg">
                             <UserAvatar
                               profilePicURL={currentUser.profilePicURL}
                               styleForUserAvatar={{
@@ -221,68 +221,71 @@ const ChatBox = () => {
                                 fontSize: "1em",
                               }}
                             />
-                          </Link>
+                          </div>
+                          <div className="msg_cotainer">
+                            {chat.message}
+                            <span className="msg_time">
+                              {timeAgo.format(new Date(chat.time))}
+                            </span>
+                          </div>
                         </div>
-                        <div className="msg_cotainer">
-                          {chat.message}
-                          <span className="msg_time">
-                            {timeAgo.format(new Date(chat.time))}
-                          </span>
-                        </div>
+                      );
+                    }
+                  })}
+                  {!!isMessageTyping && (
+                    <div className="d-flex justify-content-start mb-4">
+                      <div className="img_cont_msg">
+                        <Link
+                          to={`/profile/${currentUser._id}`}
+                          title="Account"
+                        >
+                          <UserAvatar
+                            profilePicURL={currentUser.profilePicURL}
+                            styleForUserAvatar={{
+                              width: "33px",
+                              height: "33px",
+                            }}
+                            styleForDefaultUserAvatar={{
+                              fontSize: "1em",
+                            }}
+                          />
+                        </Link>
                       </div>
-                    );
-                  }
-                })}
-                {!!isMessageTyping && (
-                  <div className="d-flex justify-content-start mb-4">
-                    <div className="img_cont_msg">
-                      <Link to={`/profile/${currentUser._id}`} title="Account">
-                        <UserAvatar
-                          profilePicURL={currentUser.profilePicURL}
-                          styleForUserAvatar={{
-                            width: "33px",
-                            height: "33px",
-                          }}
-                          styleForDefaultUserAvatar={{
-                            fontSize: "1em",
-                          }}
-                        />
-                      </Link>
+                      <div className="msg_cotainer typing-section">
+                        Typing......
+                      </div>
                     </div>
-                    <div className="msg_cotainer typing-section">
-                      Typing......
-                    </div>
-                  </div>
-                )}
-              </>
-            )}
-          </>
+                  )}
+                </>
+              )}
+            </>
+          </div>
+        </div>
+        <div className="card-footer">
+          <form
+            className="input-group"
+            onSubmit={(e) => {
+              e.preventDefault();
+              sendMessage();
+            }}
+          >
+            <input
+              className="form-control type_msg"
+              placeholder="Type your message..."
+              value={textMessage}
+              onChange={(e) => setTextMessage(e.target.value)}
+              onKeyDown={onTypingHandler}
+              ref={inputRef}
+            />
+            <div className="input-group-append">
+              <button className="input-group-text send_btn" type="submit">
+                <i className="fas fa-location-arrow" />
+              </button>
+            </div>
+          </form>
         </div>
       </div>
-      <div className="card-footer">
-        <form
-          className="input-group"
-          onSubmit={(e) => {
-            e.preventDefault();
-            sendMessage();
-          }}
-        >
-          <input
-            className="form-control type_msg"
-            placeholder="Type your message..."
-            value={textMessage}
-            onChange={(e) => setTextMessage(e.target.value)}
-            onKeyDown={onTypingHandler}
-            ref={inputRef}
-          />
-          <div className="input-group-append">
-            <button className="input-group-text send_btn" type="submit">
-              <i className="fas fa-location-arrow" />
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
+    </OutsideClickHandler>
   );
 };
 
